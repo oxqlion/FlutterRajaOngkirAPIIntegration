94a1b17fe4f571316a22d7d3d1ab4d1f4e8fd088
oxqlion <rafinaya1127@gmail.com>
2024-12-12 10:49:54 +0700

    update: added model, progress repository

---
+++ lib/model/model.dart
@@ -0,0 +1,4 @@
+import 'package:equatable/equatable.dart';
+
+part 'city.dart';
+part 'province.dart';
---
+++ lib/shared/const.dart
@@ -0,0 +1,6 @@
+part of 'shared.dart';
+
+class Const {
+  static String baseUrl = 'api.rajaongkir.com';
+  static String apiKey = 'd3c710b633fe91e49b2dc6a77fcee5de';
+}
